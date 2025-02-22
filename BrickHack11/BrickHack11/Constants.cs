9ca4765f85ecd755642ac60d5d2d02ba19233c37
jwh1000 <jwh1000@rit.edu>
2025-02-22 14:00:05 -0500

    some bullet pattern changes, bullet change

---
+++ BrickHack11/BrickHack11/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace BrickHack11;
+
+public static class Constants
+{
+    public const int ScreenWidth = 1920;
+    public const int ScreenHeight = 1080;
+}
--- BrickHack11/BrickHack11/Game1.cs
+++ BrickHack11/BrickHack11/Game1.cs
@@ -2,8 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-
-
 namespace BrickHack11
 {
     enum GameState
