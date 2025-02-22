a8566828058c77ddea64f48b2a783316d3f6763d
Jarod Rowland <78932041+jar8282@users.noreply.github.com>
2025-02-22 12:30:21 -0500

    init playerobject

---
+++ BrickHack11/BrickHack11/PlayerObject.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BrickHack11 
+{
+    class PlayerObject : GameObject
+    {
+        public PlayerObject(Texture2D spriteSheet, Rectangle position, Rectangle spriteFrame) : base(spriteSheet, position, spriteFrame)
+        {
+
+        }
+    }
+
+    
+
+}
