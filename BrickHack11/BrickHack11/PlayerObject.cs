a3227864a939a6a670a0b81e3297c173914dc996
Jarod Rowland <78932041+jar8282@users.noreply.github.com>
2025-02-22 12:41:41 -0500

    create playerObj movement update

--- BrickHack11/BrickHack11/PlayerObject.cs
+++ BrickHack11/BrickHack11/PlayerObject.cs
@@ -1,16 +1,32 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace BrickHack11 
 {
     class PlayerObject : GameObject
     {
+      private float speed = 3f;
         public PlayerObject(Texture2D spriteSheet, Rectangle position, Rectangle spriteFrame) : base(spriteSheet, position, spriteFrame)
         {
+        }
 
+        public void Update()
+        {
+          KeyboardState state = Keyboard.GetState();
+          Rectangle newPos = Position;
+
+          if(state.IsKeyDown(Keys.W))
+            newPos.Y -= (int)speed;
+          if(state.IsKeyDown(Keys.A))
+            newPos.X -= (int)speed;
+          if(state.IsKeyDown(Keys.S))
+            newPos.Y += (int)speed;
+          if(state.IsKeyDown(Keys.D))
+            newPos.X += (int)speed;
         }
     }
 
-    
+
 
 }
