5688b08af7c2685fba31c342e003c00c76f532f2
brimocars <bsm1875@rit.edu>
2025-02-22 21:26:08 -0500

    ui area

--- BrickHack11/BrickHack11/Constants.cs
+++ BrickHack11/BrickHack11/Constants.cs
@@ -2,6 +2,7 @@
 
 public static class Constants
 {
+    public const int ArenaWidth = 1920 / 2;
     public const int ScreenWidth = 1920;
     public const int ScreenHeight = 1080;
     public const int hitStopDelay = 12;
